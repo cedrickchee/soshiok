ff5921014a73c400404f78df6659a2265a1dde62
cedrickchee <cedrick.chee@gmail.com>
2017-06-12 08:57:52 +0800

    Add a new page to shows tags
    User can tag (categorize) a store (content). A page to display a list
    of tags and its count nicely.
    
    - New MongoDB schema static model method to get tag list using custom aggregations
    - New controller to get stores by tag

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -91,3 +91,9 @@ exports.getStoreBySlug = async (req, res, next) => {
   }
   res.render('store', { store, title: store.name });
 };
+
+exports.getStoresByTag = async (req, res) => {
+  const tags = await Store.getTagsList();
+  const tag = req.params.tag;
+  res.render('tag', { tags, title: 'Tags', tag });
+}
