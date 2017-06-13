d1bfaebd626ff7f9d54911c3dd96ee53d14f9f39
cedrickchee <cedrick.chee@gmail.com>
2017-06-13 23:59:44 +0800

    Fix typo for package name

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 const md5 = require('md5');
 const validator = require('validator');
 const mongodbErrorHandler = require('mongoose-mongodb-errors');
-const passportLocalMongoose = require('password-local-mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new Schema({
   email: {
