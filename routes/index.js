561bf13a199ae20f4344a319a590735073e4dd72
cedrickchee <cedrick.chee@gmail.com>
2017-06-15 23:37:45 +0800

    Add screen to edit user account

--- routes/index.js
+++ routes/index.js
@@ -44,4 +44,7 @@ router.post('/register',
 
 router.get('/logout', authController.logout);
 
+router.get('/account', authController.isLoggedIn, userController.account);
+router.post('/account', catchErrors(userController.updateAccount));
+
 module.exports = router;
